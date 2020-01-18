d7ec6cc9afd8e6b7cf349c8fe7e1ea9602c85839
fakherka <47859447+fakherka@users.noreply.github.com>
2020-01-18 11:26:17 +0100

    Add files via upload

---
+++ GoMycode/Gmc/js/main.js
@@ -0,0 +1,17 @@
+alert("It s just the beginning")
+function makebold(){
+    document.getElementById('baa').style.fontWeight='bold'
+}
+function makeitalic(){
+    document.getElementById('baa').style.fontStyle='italic'
+}
+function makeunderlined(){
+    document.getElementById('baa').style.textDecoration='underline'
+}
+function changefont(){
+    document.getElementById('baa').style.fontFamily=document.getElementById('fonts').value
+}
+function changesize(){
+    document.getElementById('baa').style.fontSize=document.getElementById('size').value
+}
+
